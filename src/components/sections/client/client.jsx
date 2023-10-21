021bd1b5aee9fce52154aeda87763b7260e3a3f1
mozahid <ce.muzahid@gmail.com>
2023-10-21 08:14:22 +0600

    update client feature

--- src/components/sections/client/client.jsx
+++ src/components/sections/client/client.jsx
@@ -15,33 +15,12 @@ const Client = () => {
             <span className="">Designation</span>
           </div>
         </div>
-        <div className="rating">
-          <input
-            type="radio"
-            name="rating-2"
-            className="mask mask-star-2 bg-orange-400"
-          />
-          <input
-            type="radio"
-            name="rating-2"
-            className="mask mask-star-2 bg-orange-400"
-            checked
-          />
-          <input
-            type="radio"
-            name="rating-2"
-            className="mask mask-star-2 bg-orange-400"
-          />
-          <input
-            type="radio"
-            name="rating-2"
-            className="mask mask-star-2 bg-orange-400"
-          />
-          <input
-            type="radio"
-            name="rating-2"
-            className="mask mask-star-2 bg-orange-400"
-          />
+        <div className="space-x-1">
+          <i className="fa-sharp fa-solid fa-star"></i>
+          <i className="fa-sharp fa-solid fa-star"></i>
+          <i className="fa-sharp fa-solid fa-star"></i>
+          <i className="fa-sharp fa-solid fa-star"></i>
+          <i className="fa-sharp fa-solid fa-star"></i>
         </div>
       </div>
       <p className="px-6 py-6">
